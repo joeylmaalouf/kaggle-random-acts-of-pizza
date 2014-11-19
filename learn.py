dc1ca1ffb2a43760907d930a10ae515268bb09fa
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-11-19 10:20:41 -0500

    more issues

--- learn.py
+++ learn.py
@@ -1,7 +1,13 @@
 import json
 
+
+def unistring(s):
+    return str(s).encode("ascii", "ignore").decode()
+
 # import JSON data as a list of dicts, each of which is a data point
 with open("train.json") as open_file:
-    file_string = open_file.read().encode("ascii", "ignore").decode()
-    train = json.loads(file_string)
-print(train)
+    train = json.loads(unistring(open_file.read()))
+#print(str(train).encode("ascii", "ignore").decode())
+for d in train:
+    for i in d:
+        print(unistring(i))
