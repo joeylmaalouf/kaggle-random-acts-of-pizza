c5974b185e98ec23fb43791549d0f48c47d5d022
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-11-22 20:26:32 -0500

    fixed encoding issues

--- learn.py
+++ learn.py
@@ -5,9 +5,8 @@ def unistring(s):
     return str(s).encode("ascii", "ignore").decode()
 
 # import JSON data as a list of dicts, each of which is a data point
-with open("train.json") as open_file:
+with open("tmp.json") as open_file:
     train = json.loads(unistring(open_file.read()))
-#print(str(train).encode("ascii", "ignore").decode())
 for d in train:
-    for i in d:
-        print(unistring(i))
+    for k in d:
+        print(unistring(k), ":", unistring(d[k]))
