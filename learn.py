a5c0cfd4445519308d98b96900328b04c6c77959
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-12-02 18:05:54 -0500

    little things

--- learn.py
+++ learn.py
@@ -5,8 +5,8 @@ def unistring(s):
     return str(s).encode("ascii", "ignore").decode()
 
 # import JSON data as a list of dicts, each of which is a data point
-with open("tmp.json") as open_file:
+with open("train.json") as open_file:
     train = json.loads(unistring(open_file.read()))
-for d in train:
-    for k in d:
-        print(unistring(k), ":", unistring(d[k]))
+# for d in train:
+#     for k in d:
+#         print(unistring(k), ":", unistring(d[k]))
