de886c3d498af81ec8b2112f960c2335440b0e2e
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-12-02 18:41:09 -0500

    relevant data functions

--- learn.py
+++ learn.py
@@ -4,9 +4,29 @@ import json
 def unistring(s):
     return str(s).encode("ascii", "ignore").decode()
 
-# import JSON data as a list of dicts, each of which is a data point
-with open("train.json") as open_file:
-    train = json.loads(unistring(open_file.read()))
-# for d in train:
-#     for k in d:
-#         print(unistring(k), ":", unistring(d[k]))
+
+def relevant_data_string(d):
+    return pad_num_str(d["requester_account_age_in_days_at_request"], 12, 6) \
+        + "     " \
+        + pad_num_str(d["requester_upvotes_minus_downvotes_at_request"], 6, 0) \
+        + "       " \
+        + str(d["requester_received_pizza"])
+
+
+def pad_num_str(s, N, n):
+    return ("%0"+str(N)+"."+str(n)+"f") % float(s)
+
+
+def main():
+    # import JSON data as a list of dicts, each of which is a data point
+    with open("train.json") as open_file:
+        train = json.loads(unistring(open_file.read()))
+    print("account age      up - down    recieved pizza?")
+    for d in train:
+        print(relevant_data_string(d))
+    #     for k in d:
+    #         print(unistring(k), ":", unistring(d[k]), "\n")
+
+
+if __name__ == "__main__":
+    main()
