86156938f6db590876bf11ad19140c4f87f4c325
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-12-02 19:46:15 -0500

    attempted plotting

--- learn.py
+++ learn.py
@@ -1,4 +1,7 @@
 import json
+import matplotlib.pyplot as plt
+# import numpy as np
+# import seaborn as sns
 
 
 def unistring(s):
@@ -17,15 +20,27 @@ def pad_num_str(s, N, n):
     return ("%0"+str(N)+"."+str(n)+"f") % float(s)
 
 
+def scatterplot(data, key1i, key2i, key3o):
+    dataX = []
+    dataY = []
+    for d in data:
+        dataX.append(float(d[key1i]))
+        dataY.append(float(d[key2i]))
+    plt.plot(dataX, dataY)
+
+
 def main():
     # import JSON data as a list of dicts, each of which is a data point
     with open("train.json") as open_file:
         train = json.loads(unistring(open_file.read()))
-    print("account age      up - down    recieved pizza?")
-    for d in train:
-        print(relevant_data_string(d))
-    #   for k in d:
-    #       print(unistring(k), ":", unistring(d[k]), "\n")
+#   print("account age      up - down    recieved pizza?")
+#   for d in train:
+#       print(relevant_data_string(d))
+#       for k in d:
+#           print(unistring(k), ":", unistring(d[k]), "\n")
+    scatterplot(train, "requester_account_age_in_days_at_request",
+                "requester_upvotes_minus_downvotes_at_request",
+                "requester_received_pizza")
 
 
 if __name__ == "__main__":
