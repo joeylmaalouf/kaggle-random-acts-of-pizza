517db89dace9e654d31c476c435aad84fc419e27
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-12-09 15:54:46 -0500

    commented everything properly

--- learn.py
+++ learn.py
@@ -15,6 +15,11 @@ def unistring(s):
 
 def process_data(data_json, key1, renamed1, key2, renamed2, key3, renamed3,
                  key4, renamed4, key5, renamed5, key6, renamed6):
+    """Converts our list of dictionaries to a dictionary of lists,
+    while processing some of the data points (e.g. converting number strings
+    to ints and float, as well as lowercasing strings). Also uses renamed keys
+    instead of the very long default ones.
+    """
     val1 = []
     val2 = []
     val3 = []
@@ -27,7 +32,7 @@ def process_data(data_json, key1, renamed1, key2, renamed2, key3, renamed3,
         val3.append(int(d[key3]))
         val4.append(d[key4].lower())
         val5.append(d[key5].lower())
-        val6.append(str(d[key6]) == "True")
+        val6.append(str(d[key6]) == "true" or str(d[key6]) == "True")
     return {renamed1: val1,
             renamed2: val2,
             renamed3: val3,
@@ -37,11 +42,16 @@ def process_data(data_json, key1, renamed1, key2, renamed2, key3, renamed3,
 
 
 def get_best_fit_poly(data_dict, x, y, deg):
+    """Finds the coefficients of the polynomial function for the best fit line.
+    """
     coeffs = np.polyfit(x=data_dict[x], y=data_dict[y], deg=deg)
     return np.poly1d(coeffs)
 
 
 def scatterplot(data_dict):
+    """Visualizes account age versus karma on the x and y axes, and whether
+    or not the requester was given a pizza as the green or red point color.
+    """
     for i in range(len(data_dict["age"])):
         formatting = "." + ("g" if data_dict["result"][i] else "r")
         plt.plot(data_dict["age"][i], data_dict["karma"][i], formatting)
@@ -65,6 +75,8 @@ def will_reciprocate(data_dict, index):
 
 
 def print_data(data_dict):
+    """Prints the data for examining or debugging purposes.
+    """
     print("id, age, karma, reciprocal, result")
     for i in range(len(data_dict["id"])):
         print(
@@ -77,7 +89,12 @@ def print_data(data_dict):
 
 
 def main(argv):
-    # import JSON data as a list of dicts, each of which is a data point
+    """The main program itself. Imports JSON data as a list of dictionaries,
+    each of which is a data point (a pizza request). Then makes predictions
+    as to whether or not the request would be fulfilled based on whether or
+    not they offered to reciprocate, as well as whether or not they had more
+    karma than the corresponding point on the best-fit line.
+    """
     with open("train.json") as open_file:
         train = json.loads(unistring(open_file.read()))
     data_dict = process_data(train,
