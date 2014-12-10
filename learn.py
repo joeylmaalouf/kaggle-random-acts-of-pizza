436fc203541bf8c9b60ee20670ff5ce773b5a9c9
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-12-09 19:11:13 -0500

    swept the polynomial parameters

--- learn.py
+++ learn.py
@@ -42,21 +42,22 @@ def process_data(data_json, key1, renamed1, key2, renamed2, key3, renamed3,
             renamed6: val6}
 
 
-def get_best_fit_poly(data_dict, x, y, deg):
+def get_best_fit_poly_coeffs(data_dict, x, y, deg):
     """Finds the coefficients of the polynomial function for the best fit line.
     """
     coeffs = np.polyfit(x=data_dict[x], y=data_dict[y], deg=deg)
-    return np.poly1d(coeffs)
+    return coeffs
 
 
-def scatterplot(data_dict):
+def scatter_plot(data_dict):
     """Visualizes account age versus karma on the x and y axes, and whether
     or not the requester was given a pizza as the green or red point color.
     """
     for i in range(len(data_dict["age"])):
         formatting = "." + ("g" if data_dict["result"][i] else "r")
         plt.plot(data_dict["age"][i], data_dict["karma"][i], formatting)
-    poly = get_best_fit_poly(data_dict, "age", "karma", 1)
+    coeffs = get_best_fit_poly_coeffs(data_dict, "age", "karma", 1)
+    poly = np.poly1d(coeffs)
     plt.plot(data_dict["age"], poly(data_dict["age"]))
     plt.show()
 
@@ -87,6 +88,36 @@ def print_data(data_dict):
             will_reciprocate(data_dict, i),
             data_dict["result"][i]
         )
+
+
+def train_acc(data_train, coeffs, multiplier):
+    """Determines the accuracy of a set of coefficients
+    (with a multiplier) on the training data.
+    """
+    poly = np.poly1d([i*multiplier for i in coeffs])
+    predictions_train = [(poly(data_train["age"][i]) < data_train["karma"][i])
+                         or will_reciprocate(data_train, i)
+                         for i in range(len(data_train["id"]))]
+    correct = 0
+    for i in range(len(predictions_train)):
+        if predictions_train[i] == data_train["result"][i]:
+            correct += 1
+    accuracy = correct / len(predictions_train) * 100
+    return accuracy
+
+
+def sweep_coeff(data_train, coeffs):
+    """Sweep the coefficients to see which multiplier gives the best accuracy.
+    """
+    best_m = 1
+    best_a = 0
+    for m in np.arange(0.0, 100.0, 0.1):
+        acc = train_acc(data_train, coeffs, m)
+        if acc > best_a:
+            best_m = m
+            best_a = acc
+            print("New best multiplier:", best_m, "with an accuracy of", best_a)
+    return [i*best_m for i in coeffs]
 
 
 def main(argv):
@@ -127,18 +158,12 @@ def main(argv):
                              "request_text_edit_aware",
                              "body")
     # print_data(data_dict)
-    # scatterplot(data_dict)
-    poly = get_best_fit_poly(data_train, "age", "karma", 1)
-
-    predictions_train = [(poly(data_train["age"][i]) < data_train["karma"][i])
-                         or will_reciprocate(data_train, i)
-                         for i in range(len(data_train["id"]))]
-    correct = 0
-    for i in range(len(predictions_train)):
-        if predictions_train[i] == data_train["result"][i]:
-            correct += 1
-    accuracy = correct / len(predictions_train) * 100
-    print("Training accuracy:", accuracy, "%")
+    # scatter_plot(data_dict)
+    coeffs = get_best_fit_poly_coeffs(data_train, "age", "karma", 1)
+    print("Initial coefficients:", coeffs)
+    best_coeffs = sweep_coeff(data_train, coeffs)
+    poly = np.poly1d(best_coeffs)
+    print("Best coefficients:", best_coeffs)
 
     predictions_test = [(poly(data_test["age"][i]) < data_test["karma"][i])
                         or will_reciprocate(data_test, i)
